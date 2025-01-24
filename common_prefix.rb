7966e1707f654f827ce72c392a2d701353830a86
BigBrad12 <bradslater12@gmail.com>
2025-01-25 00:52:20 +0200

    comments

--- common_prefix.rb
+++ common_prefix.rb
@@ -1,4 +1,9 @@
-require 'pp'
+# The longest common prefix may have you scratching your head a lot, doing some crazy string manipulation and looping things
+# like you're on a roller coaster, struggling for an hour scratching your head just like I was. Maybe you're a chad and found it so
+# easy. Either way, it is stupid easy one you realize that all you have to do is sort the array of strings alphabetically. Once done, simply
+# compare the first and last strings to find the longest common prefix, or not as there may not be one.
+
+require 'pp' # A massive one. I don't remember why I required this, but it's useful for printing out hash maps nicely in your terminal.
 
 def longest_common_prefix(strs)
   strings = strs.sort
--- two_sum.rb
+++ two_sum.rb
@@ -1,40 +1,17 @@
-# Initialize an empty hash map for student grades
-# grades = {}
-#
-# 1. Insert (add) student grades
-# grades['Alice'] = 95   # Add Alice's grade
-# grades['Bob'] = 88     # Add Bob's grade
-# grades['Charlie'] = 72 # Add Charlie's grade
-# puts "Grades after insertion: #{grades}"
-#
-# 2. Lookup a grade (quick access by key)
-# Alice_grade = grades['Alice']
-# puts "Alice's grade: #{alice_grade}"
-#
-# 3. Update a grade (overwrite the value for a key)
-# grades['Bob'] = 92
-# puts "Grades after updating Bob's grade: #{grades}"
-#
-# 4. Delete a student's grade
-# grades.delete('Charlie')
-# puts "Grades after deleting Charlie: #{grades}"
-#
-# 5. Check if a student exists
-# if grades.key?('Alice')
-#   puts "Alice's grade exists in the system!"
-# else
-#   puts "Alice's grade does not exist."
-# end
-
-# 6. Iterating over the hash
-# grades.each do |name, grade|
-#   puts "#{name} has a grade of #{grade}."
-# end
+# Hash maps are the way to go here, especially since they are likely well used and useful in the real world.
+# Initially I tried using a nested loop, this is bad for performance. Even if you optimize a nested loop and gather the data you need in
+# the map, it's still going to be a pain to extract the answer.
+# There is one key to this, and that is realizing the solution doesn't necessarily lie in code itself, but in algebra.
+# For each number you're looping on in the array, calculate target - number = the number you are looking for. Having the number that will
+# satisfy the solution for each number you're looping on greatly simplifies the problem.
+# Then check your map to see if that number is present, and if it is grab it's index and use the current numbers index and return those two
+# indices.
 
 def two_sum(nums, target)
   hash = {}
   nums.each_with_index do |num, index|
-    return [hash[target - num], index] unless hash[target - num].nil?
+    complement = target - num
+    return [hash[complement], index] if hash[complement]
 
     hash[num] = index
   end
